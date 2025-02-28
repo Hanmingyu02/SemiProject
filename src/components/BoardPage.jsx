a1a3fc063a3143864f977734cb22d0f155c1aafe
Hanmingyu02 <hmg0835@naver.com>
2025-03-01 00:39:50 +0900

    /

--- src/components/BoardPage.jsx
+++ src/components/BoardPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BoardPage() {
     return (
         <div>
-            <div>자유게시판</div>
+            <div>자유게시판123</div>
         </div>
     );
 }

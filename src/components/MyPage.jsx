e0e31126f63d23ac7e7a80d6a08aec38593155c7
Hanmingyu02 <hmg0835@naver.com>
2025-03-06 14:03:49 +0900

    마이페이지 초안

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,9 +1,30 @@
-import React from "react";
-
+import React from 'react';
+import { Container, Card, Button } from 'react-bootstrap';
 export default function MyPage() {
     return (
-        <div>
-            <div>마이페이지123123</div>
-        </div>
+        <Container>
+            <div className="mypageTop centered">My Page</div>
+            <Card style={{ width: '18rem' }} className="centered">
+                <img src="/images/person.svg" alt="profile" className="mypageImg" s />
+                <Card.Body>
+                    <div className="mypageBody mb-4">User Information</div>
+                    <div className="mb-1">
+                        <Button variant="outline-danger" style={{ width: '235px' }}>
+                            Change Email
+                        </Button>
+                    </div>
+                    <div className="mb-1">
+                        <Button variant="outline-primary" style={{ width: '235px' }}>
+                            Change Name
+                        </Button>
+                    </div>
+                    <div className="mb-1">
+                        <Button variant="outline-warning" style={{ width: '235px' }}>
+                            View reservation information
+                        </Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
